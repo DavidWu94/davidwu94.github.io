a2d77ca6dcad50ac0c66a329b72030b723199d16
DavidWu94 <154606523+DavidWu94@users.noreply.github.com>
2023-12-31 14:20:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function getValue() {
+    const inputBoxcod = document.getElementById ( 'cod');
+    const inputBoxpw = document.getElementById ( 'pw');
+    const valuecod = inputBoxcod.value;
+    const valuepw = inputBoxpw.value;
+    
+    
+    
+    const postData = { key: 'value' };
+
+fetch('http://localhost:3000/submit', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(postData)
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error('Error:', error));
+  }
+  
