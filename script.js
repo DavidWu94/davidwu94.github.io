ff860eb204a564ff1d1ee0b6ce3d941d89b0e24a
tobydog0501 <tobydog0501@gmail.com>
2023-12-31 18:22:51 +0800

    just a little test

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ function getValue() {
     
     
     const postData = { key: 'value' };
-
-fetch('http://localhost:3000/submit', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(postData)
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
+	/*
+	fetch('http://localhost:3000/submit', {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json'
+	  },
+	  body: JSON.stringify(postData)
+	})
+	.then(response => response.json())
+	.then(data => console.log(data))
+	.catch(error => console.error('Error:', error));
+	*/
+	const receivedData = "NONE";
+	window.location = `${window.location.origin}/employee/employee.html?data=${receivedData}`
   }
   
