858a4cb1407d1078ea3103d133d83d94b646add1
tobydog0501 <tobydog0501@gmail.com>
2024-01-01 15:42:51 +0800

    主葉面

--- employee/script.js
+++ employee/script.js
@@ -2,7 +2,23 @@ window.onload = ()=>{
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	const data = urlParams.get('data')
+	/*
 	if(data===null){
 		window.location = window.location.origin;
-	}
+	}*/
+}
+
+
+function newdayoff(){
+	fetch(`${window.location.origin}/employee/templates/newdayoff.html`)
+		.then((res)=>res.text().then(text=>{
+			document.getElementById("full").innerHTML = text;
+		}))
+}
+
+function viewData(){
+	fetch(`${window.location.origin}/employee/templates/viewdata.html`)
+		.then((res)=>res.text().then(text=>{
+			document.getElementById("full").innerHTML = text;
+		}))
 }
