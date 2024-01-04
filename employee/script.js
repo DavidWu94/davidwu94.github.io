2c11364f586c4b35930c0183d9aef72db4dc7212
tobydog0501 <tobydog0501@gmail.com>
2024-01-04 21:12:22 +0800

    未登入自動重導向

--- employee/script.js
+++ employee/script.js
@@ -2,10 +2,10 @@ window.onload = ()=>{
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	const data = urlParams.get('data')
-	/*
+	
 	if(data===null){
 		window.location = window.location.origin;
-	}*/
+	}
 }
 
 function newdayoff(){
