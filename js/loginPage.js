11b38e9a36283cf2e169552a2b4100aaa5f6557d
Tobydog0501 <tobydog0501@gmail.com>
2024-06-02 18:30:50 +0800

    login interaction

--- js/loginPage.js
+++ js/loginPage.js
@@ -1,7 +1,7 @@
 $(function() {
 	$("#login").on("click",function(){
 		$.ajax({
-			url: 'http://127.0.0.1:3000/login',
+			url: 'https://yoconcorporation.loophole.site/login',
 			type: 'POST',
 			dataType: 'json',
 			headers: {
@@ -12,9 +12,8 @@ $(function() {
 				pwd:$("#pw").val()
 			}),
 		}).then(res=>{
-			// console.log(res)
 			document.cookie = `session=${res["sessionKey"]};`;
-			window.location = `${window.location.origin}/${res["type"]=="employee"?"employee/employee.html":res["type"]=="admin"?"admin/admin.html":""}`
+			// window.location = `${window.location.origin}/${res["type"]=="employee"?"employee/employee.html":res["type"]=="admin"?"admin/admin.html":""}`
 		});
 	});
 });
