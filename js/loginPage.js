2e1140540ae30a51caa60915ea77f10686d849bc
Tobydog0501 <tobydog0501@gmail.com>
2024-07-06 12:07:06 +0800

    read cookie

--- js/loginPage.js
+++ js/loginPage.js
@@ -1,3 +1,17 @@
+function readCookie(key){
+	const cookieString = document.cookie;
+	var cookieData = {}
+	cookieString.split("; ").forEach((val,i,cookieList)=>{
+		let a = val.split("=");
+		cookieData[a[0]] = a[1];
+	});
+	if(cookieData[key] == undefined){
+		return null;
+	}else{
+		return cookieData[key];
+	}
+}
+
 $(function() {
 	$("#login").on("click",function(){
 		$.ajax({
