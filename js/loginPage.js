e24d36e2bd70ec34060d8af87b5d1c5e083fcaf6
Tobydog0501 <tobydog0501@gmail.com>
2024-08-07 17:33:06 +0800

    redirect fix

--- js/loginPage.js
+++ js/loginPage.js
@@ -23,7 +23,7 @@ $(function() {
 			document.cookie = `session=${res["sessionKey"]};`;
 			addCookie("session",res["sessionKey"]);
 			addCookie("id",$("#code").val());
-			window.location = `${window.location.origin}/${res["accountType"]=="employee"?"employee/employee.html":res["accountType"]=="admin"?"admin/admin.html":""}`
+			window.location = `${window.location.origin}/${res["accountType"]=="employee"?"employee/employee.html":res["accountType"]=="admin"?"admin/adminMainPage.html":""}`
 		}).catch(rej=>{
 			alert("帳號或密碼錯誤");
 		});
