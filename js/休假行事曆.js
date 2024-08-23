d39b5eade5516e3e8797463bf9ab6dbe17bb646d
DavidWu <davidwu010036@gmail.com>
2024-08-23 15:42:32 +0800

    價單審核頁面大幅度修改

--- js/休假行事曆.js
+++ js/休假行事曆.js
@@ -25,12 +25,20 @@ generateCalendar = (month , year) => {
     
     let days_of_month = [31 , getFebDays(year) , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31]
 
-    calendar_days.innerHTML = ''
+    calendar_day.innerHTML = ''
 
     //抓現在日期
     let currDate = new Date()
     if (!month) month = currDate.getMonth()
     if (!year) year = currDate.getFullYear()
 
-    
+    let curr_month = `${month_names[month]}`
+    month_picker.innerHTML = curr_month
+    calendar_header_year.innerHTML = year
+
+    let first_day = new Date(year , month , 1)
+
+    for (let i = 0 ; i <= days_of_month[month] + first_day.getDate() - 1 ; i++){
+        
+    }
 }
