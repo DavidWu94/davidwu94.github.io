d9e491e0aec0eb03023c20e04e7e8b69e24b9cc6
DavidWu <davidwu010036@gmail.com>
2024-08-28 00:04:25 +0800

    價單審核只差審核後回傳功能還沒完成(還未測試)，回傳功能不會

--- js/假單審核 test.js
+++ js/假單審核 test.js
@@ -23,67 +23,148 @@ $(function() {
         }),
     }).then(res=>{
         var list = document.getElementById("list");
-        var number = document.getElementById("number");
-        var woker_name = document.getElementById("woker_name");
-        var leave = document.getElementById("leave");
-        var reason = document.getElementById("reason");
-        var time = document.getElementById("time");
-        var btn_yes = document.querySelector(".yes");
-        var btn_no = document.querySelector(".no");
-        var previous = document.getElementById("previous");
-        var next = document.getElementById("next");
-        
-        
-        
-        window.onload = function(){
-            var a = 0;
-            next.addEventListener("click", function(){
-                
-                a += 1;
-            
-                    list.innerHTML = `
-                            <table>
-                                <tr>
-                                    <th colspan="3" id="number">流水號:${a}</th>
-                                </tr>
-                                <tr>
-                                    <th></th>
-                                    <td id="woker_name">員工姓名</td>
-                                    <th></th>
-                                </tr>
-                                <tr>
-                                    <th></th>
-                                    <td id="leave">假別</td>
-                                    <th></th>
-                                </tr>
-                                <tr>
-                                    <th></th>
-                                    <td id="reason">事由</td>
-                                    <th></th>
-                                </tr>
-                                <tr>
-                                    <th></th>
-                                    <td id="time">2024/12/15/15:00-2024/12/15/18:00</td>
-                                    <th></th>
-                                </tr>
-                                <tr>
-                                    <th class="btn"><button class="yes" id="0">核准</button></th>
-                                    <th></th>
-                                    <th class="btn"><button class="no" id="0">拒絕</button></th>
-                                </tr>
-                                <tr>
-                                    <th class="btn"><button id="previous">上一個</button></th>
-                                    <th></th>
-                                    <th class="btn"><button id="next">下一個</button></th>
-                                </tr>
-                            </table>
+        let i = 0;
+
+        list.innerHTML = `
+            <table>
+                <tr>
+                    <th colspan="3" id="number">流水號:${res["date"][i]["serialnum"]}</th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="woker_name">${res["date"][i]["id"]}</td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="leave">${res["date"][i]["type"]}</td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="reason"></td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="核准()">核准</button></th>
+                    <th></th>
+                    <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="拒絕()">拒絕</button></th>
+                </tr>
+                <tr>
+                    <th class="btn"><button id="previous" onclick="上一個()">上一個</button></th>
+                    <th></th>
+                    <th class="btn"><button id="next" onclick="下一個()">下一個</button></th>
+                </tr>
+            </table>
+        `
+
+        function 下一個(){
+            a += 1;
+            list.innerHTML = `
+                <table>
+                    <tr>
+                        <th colspan="3" id="number">流水號:${res["date"][i]["serialnum"]}</th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td id="woker_name">${res["date"][i]["id"]}</td>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td id="leave">${res["date"][i]["type"]}</td>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td id="reason"></td>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="核准()">核准</button></th>
+                        <th></th>
+                        <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="拒絕()">拒絕</button></th>
+                    </tr>
+                    <tr>
+                        <th class="btn"><button id="previous" onclick="上一個()">上一個</button></th>
+                        <th></th>
+                        <th class="btn"><button id="next" onclick="下一個()">下一個</button></th>
+                    </tr>
+                </table>
+            `
+
+
+
+            function 上一個(){
+                a -= 1;
+                list.innerHTML = `
+                    <table>
+                        <tr>
+                            <th colspan="3" id="number">流水號:${res["date"][i]["serialnum"]}</th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <td id="woker_name">${res["date"][i]["id"]}</td>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <td id="leave">${res["date"][i]["type"]}</td>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <td id="reason"></td>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="核准()">核准</button></th>
+                            <th></th>
+                            <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="拒絕()">拒絕</button></th>
+                        </tr>
+                        <tr>
+                            <th class="btn"><button id="previous" onclick="上一個()">上一個</button></th>
+                            <th></th>
+                            <th class="btn"><button id="next" onclick="下一個()">下一個</button></th>
+                        </tr>
+                    </table>
                 `
-            })
         }
+    }     
         
-           
+        
     });
        
         
     });
+
+
+    function 核准(){
+        
+        let id = document.getElementById("id");
+
+    }
+
+
+    function 拒絕(){
+
+        let id = document.getElementById("id");
+
+    }
+
 });
