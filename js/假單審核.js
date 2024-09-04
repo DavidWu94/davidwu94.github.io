fb639380c58f45513c830a17e34c2c8f18eecfec
DavidWu <davidwu010036@gmail.com>
2024-09-05 00:25:28 +0800

    假單測四

--- js/假單審核.js
+++ js/假單審核.js
@@ -22,22 +22,19 @@ $(function() {
             
         }),
     }).then(res=>{
-        var list = document.getElementById("list");
-        let i = 0;
-
         list.innerHTML = `
             <table>
                 <tr>
-                    <th colspan="3" id="number">流水號:${res["date"][i]["serialnum"]}</th>
+                    <th colspan="3" id="number">流水號:${res["date"][0]["serialnum"]}</th>
                 </tr>
                 <tr>
                     <th></th>
-                    <td id="woker_name">${res["date"][i]["id"]}</td>
+                    <td id="woker_name">${res["date"][0]["id"]}</td>
                     <th></th>
                 </tr>
                 <tr>
                     <th></th>
-                    <td id="leave">${res["date"][i]["type"]}</td>
+                    <td id="leave">${res["date"][0]["type"]}</td>
                     <th></th>
                 </tr>
                 <tr>
@@ -47,13 +44,13 @@ $(function() {
                 </tr>
                 <tr>
                     <th></th>
-                    <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
+                    <td id="time">${res["date"][0]["start"]}-${res["date"][0]["end"]}</td>
                     <th></th>
                 </tr>
                 <tr>
-                    <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="yes()">核准</button></th>
+                    <th class="btn"><button class="yes" id="${res["date"][0]["serialnum"]}" onclick="yes()">核准</button></th>
                     <th></th>
-                    <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="no()">拒絕</button></th>
+                    <th class="btn"><button class="no" id="${res["date"][0]["serialnum"]}" onclick="no()">拒絕</button></th>
                 </tr>
                 <tr>
                     <th class="btn"><button id="previous" onclick="previous()">上一個</button></th>
@@ -62,10 +59,12 @@ $(function() {
                 </tr>
             </table>
         `
-        
     });
 
 });
+
+var list = document.getElementById("list");
+let i = 0;
 
 function next(){
     a += 1;
@@ -110,43 +109,43 @@ function next(){
 
 
 
-    function previous(){
-        a -= 1;
-        list.innerHTML = `
-            <table>
-                <tr>
-                    <th colspan="3" id="number">流水號:${res["date"][i]["serialnum"]}</th>
-                </tr>
-                <tr>
-                    <th></th>
-                    <td id="woker_name">${res["date"][i]["id"]}</td>
-                    <th></th>
-                </tr>
-                <tr>
-                    <th></th>
-                    <td id="leave">${res["date"][i]["type"]}</td>
-                    <th></th>
-                </tr>
-                <tr>
-                    <th></th>
-                    <td id="reason"></td>
-                    <th></th>
-                </tr>
-                <tr>
-                    <th></th>
-                    <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
-                    <th></th>
-                </tr>
-                <tr>
-                    <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="yes()">核准</button></th>
-                    <th></th>
-                    <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="no()">拒絕</button></th>
-                </tr>
-                <tr>
-                    <th class="btn"><button id="previous" onclick="previous()">上一個</button></th>
-                    <th></th>
-                    <th class="btn"><button id="next" onclick="next()">下一個</button></th>
-                </tr>
-            </table>
-        `
+function previous(){
+    a -= 1;
+    list.innerHTML = `
+        <table>
+            <tr>
+                <th colspan="3" id="number">流水號:${res["date"][i]["serialnum"]}</th>
+            </tr>
+            <tr>
+                <th></th>
+                <td id="woker_name">${res["date"][i]["id"]}</td>
+                <th></th>
+            </tr>
+            <tr>
+                <th></th>
+                <td id="leave">${res["date"][i]["type"]}</td>
+                <th></th>
+            </tr>
+            <tr>
+                <th></th>
+                <td id="reason"></td>
+                <th></th>
+            </tr>
+            <tr>
+                <th></th>
+                <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
+                <th></th>
+            </tr>
+            <tr>
+                <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="yes()">核准</button></th>
+                <th></th>
+                <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="no()">拒絕</button></th>
+            </tr>
+            <tr>
+                <th class="btn"><button id="previous" onclick="previous()">上一個</button></th>
+                <th></th>
+                <th class="btn"><button id="next" onclick="next()">下一個</button></th>
+            </tr>
+        </table>
+    `
 }     
