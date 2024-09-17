3f53e6b45b5daa907fee434d2bd7240faf7a79cd
Tobydog0501 <tobydog0501@gmail.com>
2024-09-17 20:38:22 +0800

    date->data

--- js/假單審核.js
+++ js/假單審核.js
@@ -7,9 +7,8 @@ $(function() {
         window.location = window.location.origin;
     }
     loginCheck(userId,sessionKey);
-    var list = document.getElementById("list");
 	
-        $.ajax({
+    $.ajax({
         url: `http://eucan.ddns.net:3000/query`,
         type: 'POST',
         dataType: 'json',
@@ -22,6 +21,8 @@ $(function() {
             
         }),
     }).then(res=>{
+        var list = document.getElementById("list");
+        console.log(res);
         list.innerHTML = `
             <table>
                 <tr>
@@ -29,12 +30,12 @@ $(function() {
                 </tr>
                 <tr>
                     <th></th>
-                    <td id="woker_name">${res["date"][0]["id"]}</td>
+                    <td id="woker_name">${res["data"][0]["id"]}</td>
                     <th></th>
                 </tr>
                 <tr>
                     <th></th>
-                    <td id="leave">${res["date"][0]["type"]}</td>
+                    <td id="leave">${res["data"][0]["type"]}</td>
                     <th></th>
                 </tr>
                 <tr>
@@ -44,13 +45,13 @@ $(function() {
                 </tr>
                 <tr>
                     <th></th>
-                    <td id="time">${res["date"][0]["start"]}-${res["date"][0]["end"]}</td>
+                    <td id="time">${res["data"][0]["start"]}-${res["data"][0]["end"]}</td>
                     <th></th>
                 </tr>
                 <tr>
-                    <th class="btn"><button class="yes" id="${res["date"][0]["serialnum"]}" onclick="yes()">核准</button></th>
+                    <th class="btn"><button class="yes" id="${res["data"][0]["serialnum"]}" onclick="yes()">核准</button></th>
                     <th></th>
-                    <th class="btn"><button class="no" id="${res["date"][0]["serialnum"]}" onclick="no()">拒絕</button></th>
+                    <th class="btn"><button class="no" id="${res["data"][0]["serialnum"]}" onclick="no()">拒絕</button></th>
                 </tr>
                 <tr>
                     <th class="btn"><button id="previous" onclick="previous()">上一個</button></th>
@@ -67,44 +68,58 @@ var list = document.getElementById("list");
 let i = 0;
 
 function next(){
-    i += 1;
-    list.innerHTML = `
-        <table>
-            <tr>
-                <th colspan="3" id="number">流水號:${i}</th>
-            </tr>
-            <tr>
-                <th></th>
-                <td id="woker_name">${res["date"][i]["id"]}</td>
-                <th></th>
-            </tr>
-            <tr>
-                <th></th>
-                <td id="leave">${res["date"][i]["type"]}</td>
-                <th></th>
-            </tr>
-            <tr>
-                <th></th>
-                <td id="reason"></td>
-                <th></th>
-            </tr>
-            <tr>
-                <th></th>
-                <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
-                <th></th>
-            </tr>
-            <tr>
-                <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="yes()">核准</button></th>
-                <th></th>
-                <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="no()">拒絕</button></th>
-            </tr>
-            <tr>
-                <th class="btn"><button id="previous" onclick="previous()">上一個</button></th>
-                <th></th>
-                <th class="btn"><button id="next" onclick="next()">下一個</button></th>
-            </tr>
-        </table>
-    `
+    $.ajax({
+        url: `http://eucan.ddns.net:3000/query`,
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+            account:userId,
+            cookie:sessionKey,
+            
+        }),
+    }).then(res=>{
+        i += 1;
+        list.innerHTML = `
+            <table>
+                <tr>
+                    <th colspan="3" id="number">流水號:${i}</th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="woker_name">${res["date"][i]["id"]}</td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="leave">${res["date"][i]["type"]}</td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="reason"></td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <td id="time">${res["date"][i]["start"]}-${res["date"][i]["end"]}</td>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th class="btn"><button class="yes" id="${res["date"][i]["serialnum"]}" onclick="yes()">核准</button></th>
+                    <th></th>
+                    <th class="btn"><button class="no" id="${res["date"][i]["serialnum"]}" onclick="no()">拒絕</button></th>
+                </tr>
+                <tr>
+                    <th class="btn"><button id="previous" onclick="previous()">上一個</button></th>
+                    <th></th>
+                    <th class="btn"><button id="next" onclick="next()">下一個</button></th>
+                </tr>
+            </table>
+        `
+    })
 }
 
 
