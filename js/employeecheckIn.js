ca7fb3cbbecf997da2cdd04944c6e2d1b046aec1
DavidWu <davidwu010036@gmail.com>
2025-01-27 21:28:15 +0800

    打卡扮成

--- js/employeecheckIn.js
+++ js/employeecheckIn.js
@@ -18,4 +18,66 @@ $(function() {
     $("#day").text("當前日期:" + dateStr);
     $("#time").text(dateStr + " / " + timeStr);
 
+    $.ajax({
+        url: `http://eucan.ddns.net:3000/clockin`,
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+            account:userId,
+            cookie:sessionKey,
+            type:0
+        }),
+    }).then(res => {
+        console.log(res);
+        $("#work").append(res.time);
+    })
+
 });
+
+$("#workButton").on("click",()=>{
+    const sessionKey = readCookie("session");
+    const userId = readCookie("id");
+    $.ajax({
+        url: `http://eucan.ddns.net:3000/clockin`,
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+            account:userId,
+            cookie:sessionKey,
+            type:1
+        }),
+    }).then(res => {
+        alert("上班")
+        console.log(res);
+    })
+    
+})
+
+
+$("#restButton").on("click",()=>{
+    const sessionKey = readCookie("session");
+    const userId = readCookie("id");
+    $.ajax({
+        url: `http://eucan.ddns.net:3000/clockin`,
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+            account:userId,
+            cookie:sessionKey,
+            type:-1
+        }),
+    }).then(res => {
+        alert("下班")
+        console.log(res);
+    })
+    
+})
