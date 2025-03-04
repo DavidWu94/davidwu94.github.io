dae2acf28dffb66c667ed0d65ab617dc72c6f678
DavidWu <davidwu010036@gmail.com>
2025-03-04 13:04:15 +0800

    員工端和主管端行事曆半成品

--- js/adminCalendar.js
+++ js/adminCalendar.js
@@ -1,3 +1,24 @@
 $(function () {
+    const sessionKey = readCookie("session");
+    const userId = readCookie("id");
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1; // 修正月份
+    $.ajax({
+        url: `http://eucan.ddns.net:3000/calendar`,
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({
+            account: userId,
+            cookie: sessionKey,
+            year: year,
+            month: month
+        }),
+    }).then((res) => {
+        console.log(res);
+    })
     $("#navbar-container").load("../admin/navbar.html");
 });
